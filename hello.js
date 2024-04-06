23067246eefdcdef5be3906a2310f2af3c7b568d
Harit Rajkotiya <rajkotiyaharit@gmail.com>
2024-04-06 12:50:53 +0530

    SS

--- hello.js
+++ hello.js
@@ -1,2 +1,28 @@
-console.log("harit")
-console.log("rajkotiya")
+"use strict";
+const AccountID = 123;
+let accountEmail = "harit@google.com";
+var accountpassword = "12345";
+accountCity = "Jaipur";
+// console.log(AccountID);
+// console.table([AccountID,accountEmail,accountpassword,accountCity])
+
+let a = 4;
+if (true) {
+  let a = 5;
+}
+// console.log(a);
+
+let accountstate;
+// console.log(accountstate);
+
+
+
+
+
+// DATA TYPES
+
+// alert("hello")  // we are using nodejs , not browser
+
+console.log(1 + 2);
+
+console.log("ss");
