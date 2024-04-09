e9557de8698760237d1525bd5763e45bed40d0d0
Harit Rajkotiya <rajkotiyaharit@gmail.com>
2024-04-09 17:58:35 +0530

    sxS

--- hello.js
+++ hello.js
@@ -86,6 +86,125 @@ let myobj = {
   age : 22,
 }
 
+
 // 1:52:11
+
+let user1  = {
+  email : "harit@gmail.com",
+  upi : 123
+}
+
+user2 = user1 // assigning non-premitive data used referencing in heap memory change in one will change in another also
+user2.upi = 124
+// console.log(user1)
+// console.log(user2) // non-previmitve use heap memory , 
+
+//
+// console.log(`hello my name is ${myobj.name} and my email id is ${user1.email}`)
+
+const s = new String("harit")
+// console.log(s.indexOf(''));
+
+// console.log(s.slice(-4,-1));
+
+const news = "    hdhsdsd     "
+// console.log(news)
+// console.log(news.trim())
+
+const url = "harit%20rajkotiya.com"
+// url.replace(" ","-")
+// console.log(url.replace("%20","-"));
+
+// console.log(url.includes('harit'))
+
+
+z = "a-b-c-d-e-f-g-h-i-j"
+z = z.split('-')
+// console.log(z);
+
+const hundr = 1000000
+// console.log(hundr.toLocaleString('en-US'));
+
+///
+
+
+let myDate = new Date()
+// let newdate = new Date(2023,0,23)
+// let newdate = new Date(2023,0,23,5,12,32,1000)
+let newdate = new Date()
+// console.log(newdate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(newdate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// console.log(newdate)
+// console.log(newdate.getMonth()+1)
+// console.log(newdate.getDay())
+
+// newdate.toLocaleString('defualt' , {weekday : "short"})
+
+// console.log(newdate);
+
+///
+
+
+const myarr = [1,2,3,4,5]
+const heroes = ["shaktiman","harit"]
+
+const myarr2 = new Array(1,2,3,4)
+// console.log(myarr2)
+;
+
+myarr.push(6)
+myarr.push(7)
+myarr.pop()
+// myarr.unshift(0)
+// myarr.unshift(-1)
+
+myarr.shift()
+
+// console.log(myarr.includes(3));
+
+const newarr = myarr.join()
+// console.log(typeof newarr);
+// console.log( newarr);
+// console.log(myarr);
+
+let myn1 = myarr.slice(1,3)
+
+// console.log(myn1);
+
+let myn2 = myarr.splice(1,3)
+
+// console.log(myarr); // 1 to 3 indx element will be removed
+// console.log(myn2);
+
+// myn1.push(myn2)
+
+// const all = myn1.concat(myn2)
+// console.log(all);
+
+const all_new_heroes = [...myn1,...myn2]
+// console.log(all_new_heroes);
+
+
+const ano_arr = [1,2,3,[4,5,6],7,[4,[1,2,3]]]
+
+// const real_ano = ano_arr.flat(Infinity)
+// console.log(real_ano)
+
+// console.log(Array.isArray("Hitest"));
+// console.log(Array.from("Hitest"));
+// console.log(Array.from({name : "hitesh"}));
+
+// let sc1 = 100
+// let sc2 = 200
+// let sc3 = 300
+
+// console.log(Array.of(sc1,sc2,sc3));
+
+
 
 
