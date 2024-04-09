f7914315388e376d73e50582be641b1ef586bb29
Harit Rajkotiya <rajkotiyaharit@gmail.com>
2024-04-09 16:28:57 +0530

    ss

--- hello.js
+++ hello.js
@@ -30,9 +30,62 @@ let accountstate;
 
 a = "aaa"
 let num = Number(a)
-console.log(num);
-console.log(typeof num);
+// console.log(num);
+// console.log(typeof num);
 
 //
 
 // Time :- 1:14:50
+
+let value = 3;
+let negval = -value
+// console.log(negval);
+
+
+let s1 = "hello";
+let s2 = " harit"
+// console.log(s1+s2);
+
+// console.log("1" + 1);
+// console.log("1"+2+2);
+// console.log(2+"1");
+// console.log(2+2+"1");
+
+// console.log(+true);
+// console.log(true+true+true);
+// console.log(true+3);
+// console.log(+"");
+
+let num1 , num2 , num3;
+num1 = num2 = num3 = 4;
+// console.log(num1);
+
+
+// console.log("2">1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+
+// console.log("2"==2);
+// console.log(2==="2");
+
+// const id1 = Symbol("123")
+// const id2 = Symbol("123")
+// console.log(id1==id2);
+
+const bif = 3243543553535345435355345345n
+
+const heros = ["12","323","3232"]
+
+let myobj = {
+  name : "hitesh",
+  age : 22,
+}
+
+// 1:52:11
+
+
